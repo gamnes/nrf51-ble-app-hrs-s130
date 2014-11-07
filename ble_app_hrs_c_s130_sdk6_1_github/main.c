b24baee8833c2d72374ba16eb72a20a96fe87568
Gaute Gamnes <gaute.gamnes@nordicsemi.no>
2014-11-07 14:05:35 +0100

    Make program run on pca10001 devel board

--- ble_app_hrs_c_s130_sdk6_1_github/main.c
+++ ble_app_hrs_c_s130_sdk6_1_github/main.c
@@ -64,6 +64,10 @@
 #include "nrf_delay.h"
 
 // Use only development kit, PCA10001
+#define LED_7 LED_0
+#define LED_6 LED_1
+#define LED_5 LED_1
+
 
 // Peripheral settings
 #define PERIPHERAL_ADV_LED_PIN_NO                   LED_6                                          /**< Is on when device is scanning. */
@@ -1398,8 +1402,7 @@ static void peripheral_application_timers_start(void)
 
 
 int main(void)
-{
-    
+{   
     // Initialization of various modules.
     uint32_t err_code;
     
